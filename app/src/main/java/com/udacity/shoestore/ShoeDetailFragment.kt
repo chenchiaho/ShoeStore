53004e48a7b4dfa85ef86f3d7c43e61f33b817bb
Kevin La <chenchiaho@gmail.com>
2020-11-15 13:38:16 +0800

    3rd commit

--- app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -6,12 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
 class ShoeDetailFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = ShoeDetailFragment()
-    }
 
     private lateinit var viewModel: ShoeDetailViewModel
 
@@ -19,7 +18,12 @@ class ShoeDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_shoe_detail, container, false)
+        val binding: FragmentShoeDetailBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_shoe_detail, container, false)
+        binding.buttonCancel.setOnClickListener { v: View ->
+            v.findNavController().navigate(ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListFragment())
+        }
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
